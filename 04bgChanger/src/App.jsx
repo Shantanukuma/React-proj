0ed30498babba5840cc48166bd894e60d64f5ad6
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-05-05 14:16:49 +0530

    new project

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState('yellow');////// state manage ment
+
+  // function changeBackground(color) {
+  //   setColor(color)
+  // }
+  return (
+    <div class="flex flex-row  items-center justify-center gap-10 h-screen" style={{ backgroundColor: color }}>
+      {/* <h1 class="text-3xl font-bold mb-6 text-black drop-shadow">
+        Tailwind Background Changer
+      </h1> */}
+      <button
+        onClick={() => setColor('Red')}
+        class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
+        
+      >
+        Red
+      </button>
+      <button
+        onClick={() => setColor('blue')}
+        class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
+      >
+        Blue
+      </button>
+      <button
+        onClick={() => setColor('orange')}
+        class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
+      >
+        Orange
+      </button>
+      <button
+        onClick={() => setColor('green')}
+        class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
+      >
+        Green
+      </button>
+      <button
+        onClick={() => setColor('white')}
+        class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
+      >
+        White
+      </button>
+    </div>
+  );
+}
+
+export default App;
