12c4be5b6b6de4a6f3d7ab7fe43ffdc13af5344a
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-05-06 16:16:54 +0530

    Some changes

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -4,46 +4,60 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [color, setColor] = useState('yellow');////// state manage ment
+  const [color, setColor] = useState("yellow"); ////// state manage ment
 
   // function changeBackground(color) {
   //   setColor(color)
   // }
   return (
-    <div class="flex flex-row  items-center justify-center gap-10 h-screen" style={{ backgroundColor: color }}>
+    <div
+      class="flex flex-row  items-center justify-center gap-10 h-screen"
+      style={{ backgroundColor: color }}
+    >
       {/* <h1 class="text-3xl font-bold mb-6 text-black drop-shadow">
         Tailwind Background Changer
       </h1> */}
       <button
-        onClick={() => setColor('Red')}
+        onClick={() => setColor("Red")}
         class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
-        
       >
         Red
       </button>
       <button
-        onClick={() => setColor('blue')}
+        onClick={() => setColor("blue")}
         class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
       >
         Blue
       </button>
       <button
-        onClick={() => setColor('orange')}
+        onClick={() => setColor("orange")}
         class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
       >
         Orange
       </button>
       <button
-        onClick={() => setColor('green')}
+        onClick={() => setColor("green")}
         class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
       >
         Green
       </button>
       <button
-        onClick={() => setColor('white')}
+        onClick={() => setColor("white")}
         class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
       >
         White
+      </button>
+      <button
+        onClick={() => setColor("tomato")}
+        class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
+      >
+        tomato
+      </button>
+      <button
+        onClick={() => setColor("cyan")}
+        class="bg-black text-white px-6 py-3 rounded-lg shadow cursor-pointer hover:bg-gray-700 transition"
+      >
+        Cyan
       </button>
     </div>
   );
