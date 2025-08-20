5094bb0dd10cca57e137c0bac1582e3cde2cfd38
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-08-20 22:34:52 +0530

    Added initial files

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,12 @@
+import conf from '../conf.js'
+import { Client, Account } from "appwrite";
+
+
+
+export class AuthService {}
+
+const authService = new AuthService();
+
+export default authService;
+
+
