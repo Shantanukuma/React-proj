6406ebfe46331c47dcaa7092af9ea26a660f1edb
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-08-22 12:04:10 +0530

    Redux store

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,37 @@
-
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 import './App.css'
+import {Header, Footer} from './components/index'
+
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentuser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally( () => {setLoading(false)})
+  }, [])
 
-  // console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1 className='bg-blue-600'>Blog web</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '>
+      <div className='w-full block'>
+          <Header/>
+          <main>
+            {/* <Outlet/> */}
+          </main>
+          <Footer/>
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
