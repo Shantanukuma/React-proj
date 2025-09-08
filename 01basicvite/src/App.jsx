c4cac3ad987b0569a5f2f8508b3bdae9e7637178
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-09-08 21:34:29 +0530

    Changes

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -2,14 +2,18 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import { Provider } from 'react-redux'
+import {store} from '../src/redux/store'
 
 function App() {
 
   return (
-    <div>
-      <h1 >Vite react app</h1>
-      <h1 >Vite react app</h1>
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1 >Vite react app</h1>
+        <h1 >Vite react app</h1>
+      </div>
+    </Provider>
   )
 }
 
